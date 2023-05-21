5165d27a407616bdd2a18658959aad994b4bb332
Omar Samy <107769257+OmarSamy74@users.noreply.github.com>
2023-05-22 02:15:56 +0300

    Add files via upload

---
+++ homepage_2.cshtml.cs
@@ -0,0 +1,251 @@
+using ChartExample.Models.Chart;
+using Gym_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using System.Reflection.Emit;
+
+namespace Gym_Project.Pages
+{
+    public class homepage_2Model : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public ChartJs Chart { get; set; }
+        public string ChartJson { get; set; }
+
+
+        public body_info body_info;
+        private readonly GYM_DB db;
+        string subscribtion { get; set; }
+        [BindProperty(SupportsGet = true)]
+        [ViewData]
+        public string username_coming_from_login { get; set; }
+        [ViewData]
+        public string picurl { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime enddate{ get; set; }
+
+        public homepage_2Model(ILogger<IndexModel> logger, GYM_DB db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
+
+        public void OnGet()
+        {
+            enddate= (db.getvalid_until_date(username_coming_from_login));
+
+            if (db.getprofilepiclink(username_coming_from_login) != null)
+            {
+                picurl = db.getprofilepiclink(username_coming_from_login);
+            }
+
+            body_info = new body_info(username_coming_from_login,db.getweight(username_coming_from_login),db.getheight(username_coming_from_login),db.getmusclemass(username_coming_from_login),db.getfatpercentage(username_coming_from_login));
+            int[] weights_array = new int[12];
+            int[] heights_array = new int[12];
+            int[] fp_array = new int[12];
+            int[] mm_array = new int[12];
+            DateTime dateTime= DateTime.Now;
+            int year = dateTime.Year;
+            for (int i = 0; i < 12; i++)
+            {
+                string formatted_i = (i+1).ToString("D2");
+                weights_array[i] = db.get_specif_atribute_from_bodyinfo("weight_", formatted_i,year.ToString() , username_coming_from_login);
+                heights_array[i] = db.get_specif_atribute_from_bodyinfo("height", formatted_i, year.ToString(), username_coming_from_login);
+                fp_array[i] = db.get_specif_atribute_from_bodyinfo("Fats_Percentage", formatted_i,year.ToString(), username_coming_from_login);
+                mm_array[i] = db.get_specif_atribute_from_bodyinfo("Muscles_Percentage", formatted_i, year.ToString(), username_coming_from_login);
+            }
+            string chartData = @" 
+                
+            {
+            type: 'line',
+            responsive: true,
+            data:
+            {
+            labels: ['January', 'February', 'March', 'April','May','June','July','August','September','October','November','December'],
+            datasets: [
+            {
+            label: 'Fat percentage',
+            //data: [90, 2171, 71, 17, 122, 172, 172, 172, 172, 173, 173, 173],
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+            },
+            {
+            label: 'weight',
+            //data: [190, 2171, 71, 17, 122, 172, 172, 172, 172, 173, 173, 173],
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+            },
+            {
+            label: 'Muscle mass',
+            //data: [20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9],
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+            },
+            {
+            label: 'Height',
+            //data: [50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72],
+            backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+            }
+            ]
+            },
+            options:
+            {
+            scales:
+            {
+            y: [{
+            ticks:
+            {
+            beginAtZero: true
+            }
+            }]
+            }
+            }
+            }"; //end of chartdata
+            
+            Chart = JsonConvert.DeserializeObject<ChartJs>(chartData);
+            // setting up datasets
+            Dataset dt = new Dataset();
+            Chart.data.datasets[0].data = weights_array;
+            Chart.data.datasets[1].data = heights_array;
+            Chart.data.datasets[2].data = fp_array;
+            Chart.data.datasets[3].data = mm_array;
+
+
+            ChartJson = JsonConvert.SerializeObject(Chart, new JsonSerializerSettings
+                        {
+                            NullValueHandling = NullValueHandling.Ignore,
+                        });
+        } //end of OnGet()
+        public IActionResult OnPostM1()
+        {
+
+            start_date= DateTime.Now;
+            string username = Request.Form["username"];
+            int day = start_date.Day;
+            int month = start_date.Month;
+            int year = start_date.Year;
+            string date = month+ "-" + day+ "-" + year;
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100000);
+            db.add_subscribtion_to_user(username, randomNumber,"150","1 month",date);
+            return RedirectToPage("/homepage_2", new { username_coming_from_login = username });
+        }
+        public IActionResult OnPostM3()
+        {
+            string username = Request.Form["username"];
+            start_date = DateTime.Now;
+            int day = start_date.Day;
+            int month = start_date.Month;
+            int year = start_date.Year;
+            string date = month + "-" + day + "-" + year;
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100000);
+            db.add_subscribtion_to_user(username, randomNumber, "250", "3 month", date);
+            return RedirectToPage("/homepage_2", new { username_coming_from_login = username });
+            //return Page();
+        }
+        public IActionResult OnPostM6()
+        {
+            string username = Request.Form["username"];
+
+            start_date = DateTime.Now;
+            int day = start_date.Day;
+            int month = start_date.Month;
+            int year = start_date.Year;
+            string date = month + "-" + day + "-" + year;
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100000);
+            db.add_subscribtion_to_user(username, randomNumber, "450", "6 month", date);
+            return RedirectToPage("/homepage_2", new { username_coming_from_login = username });
+            //return Page();
+
+        }
+        public IActionResult OnPostYear_subs()
+        {
+            string username = Request.Form["username"];
+
+            start_date = DateTime.Now;
+            int day = start_date.Day;
+            int month = start_date.Month;
+            int year = start_date.Year;
+            string date = month + "-" + day + "-" + year;
+            Random random = new Random();
+            int randomNumber = random.Next(1, 100000);
+            db.add_subscribtion_to_user(username, randomNumber, "850", "1 year", date);
+            return RedirectToPage("/homepage_2", new { username_coming_from_login = username });
+            //return Page();
+        }
+        public IActionResult OnPostSubmit()
+        {
+            int Cap_rate =int.Parse( Request.Form["captains"]);
+            int Fac_rate =int.Parse( Request.Form["captains"]);
+            string comment = Request.Form["comment"];
+            var username = Request.Form["username"];
+            db.add_feedback_from_user(username,Fac_rate,Cap_rate,comment);
+            return RedirectToPage("/homepage_2", new { username_coming_from_login = username });
+        }
+    }
+}
---
+++ profile.cshtml.cs
@@ -0,0 +1,97 @@
+using Gym_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Data;
+namespace Gym_Project.Pages
+{
+    public class profileModel : PageModel
+    {
+        private readonly GYM_DB db;
+        private readonly Microsoft.AspNetCore.Hosting.IHostingEnvironment hosting;
+        public DataTable specific_user_data_table;
+        public DataTable specific_user_bodydata_data_table;
+
+        [BindProperty(SupportsGet =true)]
+        public string name { get; set; }
+        public user user { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+
+        public IFormFile File { get; set; }
+         
+        private readonly ILogger<IndexModel> _logger;
+        public profileModel(ILogger<IndexModel> logger,GYM_DB db, Microsoft.AspNetCore.Hosting.IHostingEnvironment hosting)
+        {
+            _logger = logger;
+            this.db = db;
+            this.hosting = hosting;
+        }
+        public void OnGet()
+        {
+            specific_user_data_table = db.return_an_user(name);
+            specific_user_bodydata_data_table =db.return_users_bodydata(name);
+        }
+        [HttpPost]
+
+        public IActionResult OnPostSave() {
+            user=new user();
+            string filename = string.Empty; // string tha may have the file name 
+            if (File != null)
+            {
+                string images = Path.Combine(hosting.WebRootPath, "images");// getting the path part of images folder 
+                filename = File.FileName; 
+                string fullpath = Path.Combine(images,filename); // getting the whole path
+                File.CopyTo(new FileStream(fullpath,FileMode.Create));
+            }
+
+            string birthdate = Request.Form["month"] + "-" + Request.Form["day"] + "-" + Request.Form["year"];
+            user.first_name = Request.Form["firstname"];
+            user.last_name = Request.Form["lname"];
+            user.email = Request.Form["mail"];
+            user.password = Request.Form["pass"];
+            user.username= Request.Form["username"];
+            user.profileurl = filename;
+            DateTime add_date = DateTime.Now;
+            int day = add_date.Day;
+            int month = add_date.Month;
+            int year = add_date.Year;
+            string date = month + "-" + day + "-" + year;
+            db.edituser(user.first_name,user.last_name,birthdate,user.email,user.username,user.password,user.profileurl);
+            db.edituserbodydata(int.Parse(Request.Form["height"]), int.Parse(Request.Form["weight"]), int.Parse(Request.Form["muscle"]), int.Parse(Request.Form["fat"]), Request.Form["username"],date);
+            if (user.username == "Zee")
+            {
+                return RedirectToPage("/homepage_3", new { username_coming_from_login = user.username });
+
+            }
+            else if (user.username == "sarah")
+            {
+                return RedirectToPage("/homepage_4", new { username_coming_from_login = user.username });
+            }
+            else
+            {
+                return RedirectToPage("/homepage_2", new { username_coming_from_login = user.username });
+            }
+            
+        }
+        public IActionResult OnPostCancel()
+        {
+            if (Request.Form["username"] == "Zee")
+            {
+                return RedirectToPage("/homepage_3", new { username_coming_from_login = Request.Form["username"] });
+
+            }
+            else if (Request.Form["username"] == "sarah")
+            {
+                return RedirectToPage("/homepage_4", new { username_coming_from_login = Request.Form["username"] });
+            }
+            else
+            {
+                return RedirectToPage("/homepage_2", new { username_coming_from_login = Request.Form["username"] });
+            }
+        }
+
+        
+    }
+    
+
+}
